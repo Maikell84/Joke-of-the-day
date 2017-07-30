d84e62bdb28d4e2a0244af16da8a8ac38f7323d7
Michael Blum <blummichael@gmail.com>
2017-07-30 22:50:57 +0200

    Add NSFW-Display

--- src/javascripts/api.js
+++ src/javascripts/api.js
@@ -2,17 +2,49 @@
 
 app.api = {
   getJoke: function(){
+    // Ask Reddit-API for a list of jokes
+    // Unfortunately reddit doesn't let us filter out NSFW posts. So we have to do it ourselves by
+    // getting a list of jokes and take the first Joke that is SFW
+    var limit = 1;
+    var showNSFW = app.storage.settings.showNSFW.get();
+    if(!showNSFW){
+      limit = 20;
+    }
+
     $.ajax({
       method: 'GET',
-      url: "https://www.reddit.com/r/Jokes/new.json?sort=hot",
+      url: "https://www.reddit.com/r/Jokes/hot.json?sort=hot&limit="+limit,
       success: function(response){
         // console.log(response);
-        var title = response.data.children["0"].data.title;
-        var text = response.data.children["0"].data.selftext;
-        // var nsfw = response.data.children["0"].data.over_18;
+        var title;
+        var text;
+        var nsfw;
+
+        for(var i = 0; i <= limit-1; i++){
+          title = response.data.children[i].data.title;
+          text = response.data.children[i].data.selftext;
+          nsfw = response.data.children["0"].data.over_18;
+
+          // If Display of NSFW Content is ok, or the content is non-nsfw, we're done
+          if(showNSFW || !nsfw){
+            break;
+          }
+
+          if(i == (limit-1)){
+            // User doesn't want NSFW, but we could find any SFW-Joke, So we have to call the api again
+            // TODO
+          }
+        }
+
         // var id = response.data.children["0"].data.id;
         $(".joke-header").html(title);
         $(".joke-text").html(text);
+        if(nsfw){
+          $(".nsfw-icon").show();
+        }
+        else{
+          $(".nsfw-icon").hide();
+        }
       }
     });
   }
